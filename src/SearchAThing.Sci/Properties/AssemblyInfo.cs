a08bfa40fa19c677b4cf13c4898a2b1c5d3c0c25
Lorenzo Delana <oss.devel@searchathing.com>
2016-04-29 16:00:55 +0200

    assembly informations.

--- src/SearchAThing.Sci/Properties/AssemblyInfo.cs
+++ src/SearchAThing.Sci/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SearchAThing.Sci")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Classes and extension to work with scientific data and basic geometric processing")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SearchAThing.Sci")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Lorenzo Delana 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
