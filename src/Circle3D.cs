3c30f63c4b7536c5d4177146a21641ef48b427b9
Lorenzo Delana <oss.devel@searchathing.com>
2016-10-03 21:09:35 +0200

    - fixed circle by 2 tan and 1 point ( general case 4 circles ) - added circle by 1 tan 1 point with given radius ( 2 circles )

--- src/Circle3D.cs
+++ src/Circle3D.cs
@@ -73,23 +73,53 @@ namespace SearchAThing
             /// build 3d circle that tangent to lines t1,t2 and that intersects point p
             /// note: point p must contained in one of t1,t2
             /// circle will be inside region t1.V toward t2.V
+            /// they are 4 circles
             /// </summary>            
-            public Circle3D(double tol_len, Line3D t1, Line3D t2, Vector3D p)
+            public static IEnumerable<Circle3D> CirclesTan12P(double tol_len, Line3D t1, Line3D t2, Vector3D p)
             {
-                var ip = t1.Intersect(tol_len, t2);
-                var angle = t1.V.AngleRad(tol_len, t2.V);
-                var t3 = new Line3D(ip, t1.V.RotateAs(tol_len, t1.V, t2.V, .5), Line3DConstructMode.PointAndVector);
+                foreach (var da in new double[] { 0, PI / 2 })
+                {
+                    var ip = t1.Intersect(tol_len, t2);
+                    var angle = t1.V.AngleRad(tol_len, t2.V);
+                    var t3 = new Line3D(ip, t1.V.RotateAs(tol_len, t1.V, t2.V, .5, da), Line3DConstructMode.PointAndVector);
 
-                Line3D lp = null;
-                if (t1.LineContainsPoint(tol_len, p)) lp = t1;
-                else if (t2.LineContainsPoint(tol_len, p)) lp = t2;
-                else throw new Exception($"circle 2 tan 1 point : pt must contained in one of given tan");
+                    Line3D lp = null;
+                    Line3D lNp = null;
+                    if (t1.LineContainsPoint(tol_len, p)) { lp = t1; lNp = t2; }
+                    else if (t2.LineContainsPoint(tol_len, p)) { lp = t2; lNp = t1; }
+                    else throw new Exception($"circle 2 tan 1 point : pt must contained in one of given tan");
 
-                var lpp = new Line3D(p, lp.V.RotateAboutAxis(t1.V.CrossProduct(t2.V), PI / 2), Line3DConstructMode.PointAndVector);
-                var c = lpp.Intersect(tol_len, t3);
+                    var lpp = new Line3D(p, lp.V.RotateAboutAxis(t1.V.CrossProduct(t2.V), PI / 2), Line3DConstructMode.PointAndVector);
+                    var c = lpp.Intersect(tol_len, t3);
 
-                Radius = p.Distance(c);
-                CS = new CoordinateSystem3D(c, lpp.V, t2.V);// t2.Perpendicular(tol_len, c).V);
+                    var Radius = p.Distance(c);
+                    var CS = new CoordinateSystem3D(c, lpp.V, t2.V);
+
+                    yield return new Circle3D(CS, Radius);
+
+                    // mirrored addictional circle
+
+                    var mc = c.Mirror(new Line3D(p, p.Project(lNp) - p, Line3DConstructMode.PointAndVector));
+
+                    yield return new Circle3D(new CoordinateSystem3D(mc, lpp.V, t2.V), Radius);
+                }
+            }
+
+            /// <summary>
+            /// build 3d circle through point p, tangent to given t line, with given radius r            
+            /// they can be two
+            /// </summary>            
+            public static IEnumerable<Circle3D> CircleRTanP(double tol_len, Vector3D p, Line3D t, double r)
+            {
+                var pp = p.Project(t);
+                var alpha = Asin((r - pp.Distance(p)) / r);
+                var beta = PI / 2 - alpha;
+
+                var axisz = (p - pp).CrossProduct(t.V);
+
+                var t2 = new Line3D(p, t.V.RotateAboutAxis(axisz, beta), Line3DConstructMode.PointAndVector);
+
+                return CirclesTan12P(tol_len, t, t2, p).Where(w => w.Radius.EqualsTol(tol_len, r));
             }
 
             public double Area { get { return PI * Radius * Radius; } }
@@ -114,10 +144,10 @@ namespace SearchAThing
                 var origPt = new Vector3D(Radius, 0);
                 Vector3D prevPt = origPt;
 
-                yield return origPt.ToWCS(CS);         
+                yield return origPt.ToWCS(CS);
 
                 while (alpha < alpha_stop)
-                {                    
+                {
                     var nextPt = origPt.RotateAboutZAxis(alpha);
 
                     yield return nextPt.ToWCS(CS);
