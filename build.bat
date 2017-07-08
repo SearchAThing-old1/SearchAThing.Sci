aba22b5cd3a44a5a4c0d41e2d918713532c7706f
Lorenzo Delana <oss.devel@searchathing.com>
2017-07-08 17:29:51 +0200

    build

--- build.bat
+++ build.bat
@@ -29,7 +29,7 @@ REM Build
 echo
 echo "---> Build"
 
-%msbuild% SearchAThing.Sci.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+call %msbuild% SearchAThing.Sci.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
