9ed3c2c5533b3be0bac61a436ea344a6d1c79485
Lorenzo Delana <oss.devel@searchathing.com>
2017-07-09 18:16:16 +0200

    codecov through call to avoid batch exit

--- build.bat
+++ build.bat
@@ -35,6 +35,14 @@ echo "====================> Build"
 call "%msbuild%" SearchAThing.Sci.sln /t:Restore,Rebuild /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
+REM Package
+echo
+echo "====================> Package"
+
+mkdir Build
+call %nuget% pack "src\SearchAThing.Sci.csproj" -symbols -o Build -p Configuration=%config% %version%
+if not "%errorlevel%"=="0" goto failure
+
 REM Code Coverage
 echo
 echo "====================> Coverage"
@@ -46,24 +54,17 @@ call %nuget% install OpenCover -Version 4.6.519 -OutputDirectory packages
 if not "%errorlevel%"=="0" goto failure
 
 packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"packages\xunit.runner.console.2.2.0\tools\xunit.console.exe" -targetargs:".\tests\bin\Release\SearchAThing.Sci.Tests.dll -noshadow" -output:".\coverage.xml"
-rem if not "%errorlevel%"=="0" goto failure
+if not "%errorlevel%"=="0" goto failure
 
 echo "---> ensuring codecov"
-call %nuget% install Codecov -Version 1.0.1 -OutputDirectory packages
+rem call %nuget% install Codecov -Version 1.0.1 -OutputDirectory packages
+rem if not "%errorlevel%"=="0" goto failure
+call npm install codecov -g > nul
 if not "%errorlevel%"=="0" goto failure
-rem npm install codecov > null
 
 rem packages\Codecov.1.0.1\tools\codecov.exe -f coverage.xml -t %CODECOV_TOKEN%
 echo "---> running codecov -f coverage.xml"
 codecov -f coverage.xml
-if not "%errorlevel%"=="0" goto failure
-
-REM Package
-echo
-echo "====================> Package"
-
-mkdir Build
-call %nuget% pack "src\SearchAThing.Sci.csproj" -symbols -o Build -p Configuration=%config% %version%
 if not "%errorlevel%"=="0" goto failure
 
 REM EDIT: uncommen follows exit for local debug
