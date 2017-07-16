6ee06288d260e674ee2f5b49896ab330d69ca88d
Lorenzo Delana <oss.devel@searchathing.com>
2017-07-17 01:23:03 +0200

    build

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 call %NuGet% install scriptcs -Version 0.17.1 -Source https://www.myget.org/F/scriptcsnightly/api/v3/index.json
 
+dir
+
+dir packages
+
 packages\scriptcs.0.17.1\tools\scriptcs.exe -V
 
 scriptcs build.csx
