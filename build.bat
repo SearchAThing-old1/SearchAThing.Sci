afd23c628fc3d30866d03c8522f1aa071e9cfe62
Lorenzo Delana <oss.devel@searchathing.com>
2017-07-16 18:24:27 +0200

    codecov

--- build.bat
+++ build.bat
@@ -53,16 +53,13 @@ if not "%errorlevel%"=="0" goto failure
 call %nuget% install OpenCover -Version 4.6.519 -OutputDirectory packages
 if not "%errorlevel%"=="0" goto failure
 
-packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"packages\xunit.runner.console.2.2.0\tools\xunit.console.exe" -targetargs:".\tests\bin\Release\SearchAThing.Sci.Tests.dll -noshadow" -output:".\coverage.xml"
+packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"packages\xunit.runner.console.2.2.0\tools\xunit.console.exe" -excludedirs:".\src\Thirdy"  -targetargs:".\tests\bin\Release\SearchAThing.Sci.Tests.dll -noshadow" -output:".\coverage.xml"
 if not "%errorlevel%"=="0" goto failure
 
 echo "---> ensuring codecov"
-rem call %nuget% install Codecov -Version 1.0.1 -OutputDirectory packages
-rem if not "%errorlevel%"=="0" goto failure
 call npm install codecov -g > nul
 if not "%errorlevel%"=="0" goto failure
 
-rem packages\Codecov.1.0.1\tools\codecov.exe -f coverage.xml -t %CODECOV_TOKEN%
 echo "---> running codecov -f coverage.xml"
 codecov -f coverage.xml
 if not "%errorlevel%"=="0" goto failure
